f49c78e6baecdf47103e6f4191c7453bb1e110dc
Johann Feser <johannfeser@gmail.com>
2020-09-23 15:54:12 +0200

    Update UserWorker.java

--- android/src/main/java/com/feser/ycappanalytics/UserWorker.java
+++ android/src/main/java/com/feser/ycappanalytics/UserWorker.java
@@ -17,11 +17,11 @@ import com.google.gson.Gson;
 import java.util.List;
 import java.util.Locale;
 
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+import com.squareup.okhttp3.MediaType;
+import com.squareup.okhttp3.OkHttpClient;
+import com.squareup.okhttp3.Request;
+import com.squareup.okhttp3.RequestBody;
+import com.squareup.okhttp3.Response;
 
 public class UserWorker extends Worker {
 
